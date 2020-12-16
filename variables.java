7d90880f158213202e79ebc24d83ca15e0ccc173
Manan-09 <76104205+Manan-09@users.noreply.github.com>
2020-12-16 17:51:38 +0530

    Create variables.java

---
+++ variables.java
@@ -0,0 +1,14 @@
+public class Variebles
+{
+    public static void main(String args[])
+    {
+        int a=5;
+        long b=8000000000l;
+        char c='d';
+        double d=5.43;
+        float f=5.33f;
+        c++;
+        d--;
+        System.out.print(c);
+    }
+}
