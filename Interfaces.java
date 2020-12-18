269b41b2aa4de6827fa413e5d29527cb4c72dedf
Manan-09 <76104205+Manan-09@users.noreply.github.com>
2020-12-18 19:33:20 +0530

    Create Interfaces.java

---
+++ Interfaces.java
@@ -0,0 +1,43 @@
+interface writer
+{
+    void show();
+}
+class pen implements writer
+{
+    public void show()
+    {
+        System.out.println("Writing with pen.");
+    }
+}
+class pencil implements writer
+{
+    public void show()
+    {
+        System.out.println("Writing with pencil.");
+    }
+}
+class kit
+{
+    public void show(writer p)
+    {
+        p.show();
+    }
+}
+public class interfaces
+{
+    public static void main(String[] args)
+    {
+        kit k=new kit();
+        writer obj=new pen();
+        k.show(obj);
+        obj=new pencil();
+        k.show(obj);
+        obj= new writer(){
+            public void show()
+            {
+                System.out.println("Just keeping in mind");
+            }
+        };
+        obj.show();
+    }
+}
